f2a0e43ed2679629e40cf525efae10fdc7f59fb1
mbc <malcolmbalch@gmail.com>
2017-09-09 18:02:43 -0400

    White output, refactored for variable image dimension

--- m_gan.py
+++ m_gan.py
@@ -1,53 +1,65 @@
 import os
 
-from keras.layers import Reshape, InputLayer, Flatten
+from keras.layers import Reshape, InputLayer, Flatten, LeakyReLU, Activation
 from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.regularizers import L1L2
 from keras_adversarial import (AdversarialOptimizerSimultaneous, 
                                normal_latent_sampling, simple_gan,
                                gan_targets, AdversarialModel)
-from keras_adversarial.legacy import Dense
+from keras_adversarial.legacy import Dense, l1l2
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.misc import imread
 
-# d_input_shape = (150, 150, 3)
+IMAGE_DIM = 32
 
 def get_generator():
     """
     Returns the generator model
     """
+    reg = lambda: l1l2(1e-5, 1e-5)
     model = Sequential()
-    model.add(Dense(500, input_dim=150))
-    model.add(Dense(units=150*150, activation='sigmoid'))
-    model.add(Reshape((150, 150)))
+    model.add(Dense(256, input_dim=IMAGE_DIM, W_regularizer=reg()))
+    model.add(LeakyReLU(0.2))
+    model.add(Dense(512, W_regularizer=reg()))
+    model.add(LeakyReLU(0.2))
+    model.add(Dense(1024, W_regularizer=reg()))
+    model.add(LeakyReLU(0.2))
+
+    # I think this has to be done to make the discriminator work
+    model.add(Dense(np.prod((IMAGE_DIM, IMAGE_DIM)), W_regularizer=reg()))
+    model.add(Activation('sigmoid'))
+    model.add(Reshape((IMAGE_DIM, IMAGE_DIM)))
     return model
 
 def get_discriminator():
     """
     Returns the discriminator model
     """
+    reg = lambda: l1l2(1e-5, 1e-5)
     model = Sequential()
-    model.add(Flatten(name="discriminator_flatten", input_shape=(150, 150)))
-    model.add(InputLayer(input_shape=(150, 150)))
-    model.add(Dense(500))
-    model.add(Dense(units=1, activation='sigmoid'))
+    model.add(Flatten(name="discriminator_flatten", input_shape=(IMAGE_DIM, IMAGE_DIM)))
+    model.add(Dense(1024, W_regularizer=reg()))
+    model.add(LeakyReLU(0.2))
+    model.add(Dense(512, W_regularizer=reg()))
+    model.add(LeakyReLU(0.2))
+    model.add(Dense(256, W_regularizer=reg()))
+    model.add(Dense(1, W_regularizer=reg()))
+    model.add(Activation('sigmoid'))
     return model
 
 def main():
-    data_dir = "cats_150x150/"
+    data_dir = "goldens_32x32/"
     out_dir = "m_gan_out/"
-    img_w = 150
-    img_h = 150
-    epochs = 2
+    epochs = 1
     batch_size = 16 
 
     # TODO: Research why these values were chosen
     opt_g = Adam(1e-4, decay=1e-5)
     opt_d = Adam(1e-3, decay=1e-5)
     loss='binary_crossentropy'
-    latent_dim = 150
+    latent_dim = IMAGE_DIM
     adversarial_optimizer = AdversarialOptimizerSimultaneous()
 
     generator = get_generator()
@@ -78,8 +90,16 @@ def main():
     train_x = train_x / 255
 
     history = model.fit(x=train_x, y=gan_targets(train_x.shape[0]), epochs=epochs, batch_size=batch_size)    
-    zsamples = np.random.normal(size=(10, latent_dim))
-    pred = generator.predict(zsamples)
+    
+    # TODO: Investigate if this is the source of all-white image output
+    zsamples = np.random.normal(size=(10*10, latent_dim))
+    
+    '''
+    def generator_sampler():
+        return generator.predict(zsamples).reshape((10, 10, IMAGE_DIM, IMAGE_DIM))
+    '''
+    # pred = generator.predict(zsamples)
+    pred = generator.predict(zsamples).reshape((10, 10, IMAGE_DIM, IMAGE_DIM))
     for i in range(pred.shape[0]):
         plt.savefig(out_dir+str(i)+'.png')
 
